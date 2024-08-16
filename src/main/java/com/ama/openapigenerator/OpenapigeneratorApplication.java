8bc0d0ce7bea7d86bc0e9c44e235d7e224132414
Abhijit Abhi (EXTERN: CGI) <abhijit.abhi@cgi.com>
2024-08-16 14:58:51 +0200

    feat(initiate): create springBoot 3.3.2 project with gradle

---
+++ src/main/java/com/ama/openapigenerator/OpenapigeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ama.openapigenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapigeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenapigeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ama/openapigenerator/OpenapigeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ama.openapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapigeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
